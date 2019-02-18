4f52ec65070c6e3de2d9ff6a095d289cc945137e
Oleksii Subbotin <oleksiisubbotin@MacBook-Pro-Oleksii.local>
2019-02-18 13:42:28 +0200

    Initial commit

---
+++ index.php
@@ -0,0 +1,361 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Ведущая - Катя Субботина</title>
+
+    <!-- Bootstrap Core CSS -->
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom Fonts -->
+    <link href="vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+    <link href='https://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Merriweather:400,300,300italic,400italic,700,700italic,900,900italic' rel='stylesheet' type='text/css'>
+
+    <!-- Plugin CSS -->
+    <link href="vendor/magnific-popup/magnific-popup.css" rel="stylesheet">
+
+    <!-- Theme CSS -->
+    <link href="css/creative.min.css" rel="stylesheet">
+    <link href="css/my.css" rel="stylesheet">
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+</head>
+
+<body id="page-top">
+
+    <nav id="mainNav" class="navbar navbar-default navbar-fixed-top">
+        <div class="container-fluid">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                    <span class="sr-only">Toggle navigation</span> Menu <i class="fa fa-bars"></i>
+                </button>
+                <a class="navbar-brand page-scroll" href="#page-top">Катя Субботина</a>
+            </div>
+
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav navbar-right">
+                    <li>
+                        <a class="page-scroll" href="#about">Про Меня</a>
+                    </li>
+                    <li>
+                        <a class="page-scroll" href="#services">Услуги</a>
+                    </li>
+                    <li>
+                        <a class="page-scroll" href="#promovideo">Видео</a>
+                    </li>
+                    <li>
+                        <a class="page-scroll" href="#portfolio">Портфолио</a>
+                    </li>
+                    <li>
+                        <a class="page-scroll" href="#contact">Контакты</a>
+                    </li>
+                </ul>
+            </div>
+            <!-- /.navbar-collapse -->
+        </div>
+        <!-- /.container-fluid -->
+    </nav>
+
+    <header>
+        <div class="header-content">
+            <!--div class="header-content-inner">
+                <h1 id="homeHeading">Ведущая Катя Субботина</h1>
+                <hr>
+                <p>Ваш гид в праздничном настроении</p>
+                <a href="#about" class="btn btn-primary btn-xl page-scroll">Про меня</a>
+            </div-->
+        </div>
+    </header>
+
+    <section class="bg-primary" id="about">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-8 col-lg-offset-2 text-center">
+                    <h2 class="section-heading">Любите ли Вы праздники так как их люблю я?!</h2>
+                    <hr class="light">
+                    <p class="text-faded">Первый праздник я провела в 6 лет. Скамейка под орехом возле бабушкиного дворика собрала аншлаг и я сорвала овации соседей. В детском саду Дед Мороз давал мне приз, чтобы я слезла со стульчика и перестала читать стихи :-) Потом были школьные вечера, конкурсы, выпускной, КВН в универе и вожацкий лагерный нон-стоп.</p>
+                    <p class="text-faded">Сценарии, игры, музыка, сцена, микрофон - эта любовь на всю жизнь! Я счастливый человек – моя работа – это моё хобби. Мне повезло учиться у лучших ведущих Киева, заслуженных артистов Украины.</p>
+                    <p class="text-faded">Моя формула хорошего праздника – проста - он должен пройти сквозь сознание и навсегда оставить след в сердце. Иногда хороший праздник способен изменить нас, наши отношения друг с другом. Праздник наполняет наш взгляд на мир Любовью и Радостью. Каждый раз, глядя в глаза публики я ловлю себя на мысли, что веду не просто праздники - я веду души.</p>
+                    <!--<a href="#services" class="page-scroll btn btn-default btn-xl sr-button">Get Started!</a>-->
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section id="services">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12 text-center">
+                    <h2 class="section-heading">Всегда к вашим услугам</h2>
+                    <hr class="primary">
+                </div>
+            </div>
+        </div>
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-3 col-md-6 text-center">
+                    <div class="service-box">
+                        <i class="fa fa-4x fa-female text-primary sr-icons"></i>
+                        <h3>Ведущая мероприятия</h3>
+                        <p class="text-muted">Проведем Ваш праздник на самом высшем уровне</p>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-6 text-center">
+                    <div class="service-box">
+                        <i class="fa fa-4x fa-headphones text-primary sr-icons"></i>
+                        <h3>Музыкальное сопровождение</h3>
+                        <p class="text-muted">Только лучшие композиции от DJ Petrucho</p>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-6 text-center">
+                    <div class="service-box">
+                        <i class="fa fa-4x fa-info-circle text-primary sr-icons"></i>
+                        <h3>Помощь в организации</h3>
+                        <p class="text-muted">Отвечу на ваши вопросы, помогу советом.</p>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-6 text-center">
+                    <div class="service-box">
+                        <i class="fa fa-4x fa-heart text-primary sr-icons"></i>
+                        <h3>Сделано с любовью</h3>
+                        <p class="text-muted">Всегда выкладываюсь на полную</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section id="promovideo" style="background-color:black;">
+        <div class="container">
+            <div class="row">
+                    <iframe class="col-lg-12 col-md-12 col-sm-12 col-xs-12"  height="450" src="https://www.youtube.com/embed/4wRC6IFNB5o" frameborder="0" allowfullscreen></iframe>
+            </div>
+            <div class="row">
+                    <iframe class="col-lg-12 col-md-12 col-sm-12 col-xs-12"  height="450" src="https://www.youtube.com/embed/juAYep_9ADA" frameborder="0" allowfullscreen></iframe>
+            </div>
+        </div>
+    </section>
+    <section class="no-padding" id="portfolio">
+        <div class="container-fluid">
+            <div class="row no-gutter">
+                <div class="col-lg-4 col-sm-6 popup-gallery">
+                    <a href="img/portfolio/album/first/1.jpg">
+                        <img id="preview1" src="img/portfolio/album/first/preview-grey.jpg" class="img-responsive" alt="">
+                        <!--<div class="portfolio-box-caption">
+                            <div class="portfolio-box-caption-content">
+                                <div id="test" class="project-category text-faded">
+                                    Category
+                                </div>
+                                <div class="project-name">
+                                    Project Name
+                                </div>
+                            </div>
+                        </div>-->
+                    </a>
+                    <?php
+                        $dir    = 'img/portfolio/album/first';
+                        $files1 = scandir($dir);
+                        $files = array_diff($files1, array('.', '..', '1.jpg', 'preview-color.jpg', 'preview-grey.jpg'));
+                        foreach($files as $file):
+                            echo '<a href="'.$dir.'/'.$file.'"></a>';
+                        endforeach;
+                    ?>
+                </div>
+                <div class="col-lg-4 col-sm-6 popup-gallery">
+                    <a href="img/portfolio/album/second/1.jpg">
+                        <img id="preview2" src="img/portfolio/album/second/preview-grey.jpg" class="img-responsive" alt="">
+                        <!--<div class="portfolio-box-caption">
+                            <div class="portfolio-box-caption-content">
+                                <div class="project-category text-faded">
+                                    Category
+                                </div>
+                                <div class="project-name">
+                                    Project Name
+                                </div>
+                            </div>
+                        </div>-->
+                    </a>
+                    <?php
+                        $dir    = 'img/portfolio/album/second';
+                        $files1 = scandir($dir);
+                        $files = array_diff($files1, array('.', '..', '1.jpg', 'preview-color.jpg', 'preview-grey.jpg'));
+                        foreach($files as $file):
+                            echo '<a href="'.$dir.'/'.$file.'"></a>';
+                        endforeach;
+                    ?>
+                </div>
+                <div class="col-lg-4 col-sm-6 popup-gallery">
+                    <a href="img/portfolio/album/third/1.jpg">
+                        <img id="preview3" src="img/portfolio/album/third/preview-grey.jpg" class="img-responsive" alt="">
+                        <!--<div class="portfolio-box-caption">
+                            <div class="portfolio-box-caption-content">
+                                <div class="project-category text-faded">
+                                    Category
+                                </div>
+                                <div class="project-name">
+                                    Project Name
+                                </div>
+                            </div>
+                        </div>-->
+                    </a>
+                    <?php
+                        $dir    = 'img/portfolio/album/third';
+                        $files1 = scandir($dir);
+                        $files = array_diff($files1, array('.', '..', '1.jpg', 'preview-color.jpg', 'preview-grey.jpg'));
+                        foreach($files as $file):
+                            echo '<a href="'.$dir.'/'.$file.'"></a>';
+                        endforeach;
+                    ?>
+                </div>
+                <div class="col-lg-4 col-sm-6 popup-gallery">
+                    <a href="img/portfolio/album/fourth/1.jpg">
+                        <img id="preview4" src="img/portfolio/album/fourth/preview-grey.jpg" class="img-responsive" alt="">
+                        <!--<div class="portfolio-box-caption">
+                            <div class="portfolio-box-caption-content">
+                                <div class="project-category text-faded">
+                                    Category
+                                </div>
+                                <div class="project-name">
+                                    Project Name
+                                </div>
+                            </div>
+                        </div>-->
+                    </a>
+                    <?php
+                        $dir    = 'img/portfolio/album/fourth';
+                        $files1 = scandir($dir);
+                        $files = array_diff($files1, array('.', '..', '1.jpg', 'preview-color.jpg', 'preview-grey.jpg'));
+                        foreach($files as $file):
+                            echo '<a href="'.$dir.'/'.$file.'"></a>';
+                        endforeach;
+                    ?>
+                </div>
+                <div class="col-lg-4 col-sm-6 popup-gallery">
+                    <a href="img/portfolio/album/fifth/1.jpg">
+                        <img id="preview5" src="img/portfolio/album/fifth/preview-grey.jpg" class="img-responsive" alt="">
+                        <!--<div class="portfolio-box-caption">
+                            <div class="portfolio-box-caption-content">
+                                <div class="project-category text-faded">
+                                    Category
+                                </div>
+                                <div class="project-name">
+                                    Project Name
+                                </div>
+                            </div>
+                        </div>-->
+                    </a>
+                    <?php
+                        $dir    = 'img/portfolio/album/fifth';
+                        $files1 = scandir($dir);
+                        $files = array_diff($files1, array('.', '..', '1.jpg', 'preview-color.jpg', 'preview-grey.jpg'));
+                        foreach($files as $file):
+                            echo '<a href="'.$dir.'/'.$file.'"></a>';
+                        endforeach;
+                    ?>
+                </div>
+                <div class="col-lg-4 col-sm-6 popup-gallery">
+                    <a href="img/portfolio/album/six/1.jpg">
+                        <img id="preview6" src="img/portfolio/album/six/preview-grey.jpg" class="img-responsive" alt="">
+                        <!--<div class="portfolio-box-caption">
+                            <div class="portfolio-box-caption-content">
+                                <div class="project-category text-faded">
+                                    Category
+                                </div>
+                                <div class="project-name">
+                                    Project Name
+                                </div>
+                            </div>
+                        </div>-->
+                    </a>
+                    <?php
+                        $dir    = 'img/portfolio/album/six';
+                        $files1 = scandir($dir);
+                        $files = array_diff($files1, array('.', '..', '1.jpg', 'preview-color.jpg', 'preview-grey.jpg'));
+                        foreach($files as $file):
+                            echo '<a href="'.$dir.'/'.$file.'"></a>';
+                        endforeach;
+                    ?>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <!--<aside class="bg-dark">
+        <div class="container text-center">
+            <div class="call-to-action">
+                <h2>Free Download at Start Bootstrap!</h2>
+                <a href="http://startbootstrap.com/template-overviews/creative/" class="btn btn-default btn-xl sr-button">Download Now!</a>
+            </div>
+        </div>
+    </aside>-->
+
+    <section id="contact">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-8 col-lg-offset-2 text-center">
+                    <h2 class="section-heading">Всегда на связи!</h2>
+                    <hr class="primary">
+                    <p>Звоните, пишите. С радостью отвечу на все интересующие вас вопросы.<br /> Все контакты ниже.</p>
+                </div>
+            </div>
+                
+            <div class="row">
+                <div class="col-lg-3 text-center">
+                    <i class="fa fa-phone fa-3x sr-contact"></i>
+                    <p>+38(050)445-00-40</p>
+                </div>
+                <div class="col-lg-3 text-center">
+                    <i class="fa fa-facebook-square fa-3x sr-contact"></i>
+                    <p><a href="https://www.facebook.com/profile.php?id=100010847550436&fref=ts"  target="_blank">facebook.com</a></p>
+                </div>
+                <div class="col-lg-3 text-center">
+                    <i class="fa fa-vk fa-3x sr-contact"></i>
+                    <p><a href="https://vk.com/partywithkathy" target="_blank">vk.com</a></p>
+                </div>
+                <div class="col-lg-3 text-center">
+                    <i class="fa fa-envelope-o fa-3x sr-contact"></i>
+                    <p><a href="mailto:subbotina-katya@mail.ua">subbotina-katya@mail.ua</a></p>
+                </div>
+            </div>
+        </div>      
+    </section>
+    <!--Preloaded images-->
+    <div id="preload-01"></div>
+    <div id="preload-02"></div>
+    <div id="preload-03"></div>
+    <div id="preload-04"></div>
+    <div id="preload-05"></div>
+    <div id="preload-06"></div>
+    
+    <!-- jQuery -->
+    <script src="vendor/jquery/jquery.min.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="vendor/bootstrap/js/bootstrap.min.js"></script>
+
+    <!-- Plugin JavaScript -->
+    <script src="http://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.3/jquery.easing.min.js"></script>
+    <script src="vendor/scrollreveal/scrollreveal.min.js"></script>
+    <script src="vendor/magnific-popup/jquery.magnific-popup.min.js"></script>
+
+    <!-- Theme JavaScript -->
+    <script src="js/creative.js"></script>
+    <script src="js/my.js"></script>
+
+</body>
+</html>
---
+++ js/my.js
@@ -0,0 +1,31 @@
+$('#preview1').hover(function() {
+  $(this).attr('src', '/img/portfolio/album/first/preview-color.jpg');
+  $('#test').text('asdasd');
+}, function() {
+  $(this).attr('src', '/img/portfolio/album/first/preview-grey.jpg');
+});
+$('#preview2').hover(function() {
+  $(this).attr('src', '/img/portfolio/album/second/preview-color.jpg');
+}, function() {
+  $(this).attr('src', '/img/portfolio/album/second/preview-grey.jpg');
+});
+$('#preview3').hover(function() {
+  $(this).attr('src', '/img/portfolio/album/third/preview-color.jpg');
+}, function() {
+  $(this).attr('src', '/img/portfolio/album/third/preview-grey.jpg');
+});
+$('#preview4').hover(function() {
+  $(this).attr('src', '/img/portfolio/album/fourth/preview-color.jpg');
+}, function() {
+  $(this).attr('src', '/img/portfolio/album/fourth/preview-grey.jpg');
+});
+$('#preview5').hover(function() {
+  $(this).attr('src', '/img/portfolio/album/fifth/preview-color.jpg');
+}, function() {
+  $(this).attr('src', '/img/portfolio/album/fifth/preview-grey.jpg');
+});
+$('#preview6').hover(function() {
+  $(this).attr('src', '/img/portfolio/album/six/preview-color.jpg');
+}, function() {
+  $(this).attr('src', '/img/portfolio/album/six/preview-grey.jpg');
+});
